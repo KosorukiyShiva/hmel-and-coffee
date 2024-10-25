8cbdfad7330c9aef8ce25c861719d53a839024e7
KosorukiyShiva <alcatelandry8@gmail.com>
2024-10-26 02:35:39 +0300

    a coffee page was made, not of scripts but of adapting sizes

--- src/js/beer_table.js
+++ src/js/beer_table.js
@@ -1,3 +1,5 @@
+const beer_menu_sqr_lines = document.getElementById('beer_menu_sqr_lines')
+
 const beer_menu_sqr_lines_horisontal_0 = document.getElementById('beer_menu_sqr_lines_horisontal_0')
 const beer_menu_sqr_lines_horisontal_1 = document.getElementById('beer_menu_sqr_lines_horisontal_1')
 const beer_menu_sqr_lines_horisontal_2 = document.getElementById('beer_menu_sqr_lines_horisontal_2')
@@ -15,6 +17,7 @@ const beer_menu_sqr_lines_vertical_4 = document.getElementById('beer_menu_sqr_li
 const beer_menu_sqr_lines_vertical_5 = document.getElementById('beer_menu_sqr_lines_vertical_5')
 const beer_menu_sqr_lines_vertical_6 = document.getElementById('beer_menu_sqr_lines_vertical_6')
 
+const beer_menu_sqr_number_column = document.getElementById('beer_menu_sqr_number_column')
 const beer_menu_sqr_name_column = document.getElementById('beer_menu_sqr_name_column')
 const beer_menu_sqr_brewery_column = document.getElementById('beer_menu_sqr_brewery_column')
 const beer_menu_sqr_style_column = document.getElementById('beer_menu_sqr_style_column')
@@ -27,6 +30,8 @@ const beer_menu_head = document.getElementById('beer_menu_head')
 const beer_menu_sqr = document.getElementById('beer_menu_sqr')
 const beer_menu_sqr_button = document.getElementById('beer_menu_sqr_button')
 
+const adapt_coeff = Number(((prcnt_windowInnerWidth * 95.8-8) - prcnt_windowInnerWidth * 88.3)/2)
+
 beer_menu_sqr_lines_horisontal_0.style.width = `${prcnt_windowInnerWidth * 88.3}px`
 beer_menu_sqr_lines_horisontal_1.style.width = `${prcnt_windowInnerWidth * 88.3}px`
 beer_menu_sqr_lines_horisontal_2.style.width = `${prcnt_windowInnerWidth * 88.3}px`
@@ -36,6 +41,8 @@ beer_menu_sqr_lines_horisontal_5.style.width = `${prcnt_windowInnerWidth * 88.3}
 beer_menu_sqr_lines_horisontal_6.style.width = `${prcnt_windowInnerWidth * 88.3}px`
 beer_menu_sqr_lines_horisontal_7.style.width = `${prcnt_windowInnerWidth * 88.3}px`
 beer_menu_sqr_lines_horisontal_8.style.width = `${prcnt_windowInnerWidth * 88.3}px`
+
+beer_menu_sqr_lines.style.left = `${adapt_coeff}px`
 
 beer_menu.style.width = `${prcnt_windowInnerWidth * 95.8}px`
 beer_menu_head.style.left = `${(prcnt_windowInnerWidth * 95.8 - 177) / 2}px`
@@ -50,12 +57,13 @@ if (windowInnerWidth >= 500) {
     beer_menu_sqr_style_column.style.width = `${prcnt_windowInnerWidth*18}px`
     beer_menu_abv_style_column.style.width = `${prcnt_windowInnerWidth*10}px`
     beer_menu_ibu_style_column.style.width = `${prcnt_windowInnerWidth*10}px`
-    beer_menu_sqr_name_column.style.left = `${38 }px`
-    beer_menu_sqr_brewery_column.style.left = `${38 + (prcnt_windowInnerWidth*20) + 4}px`
-    beer_menu_sqr_style_column.style.left = `${38 + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4}px`
-    beer_menu_abv_style_column.style.left = `${38 + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*18) + 4}px`
-    beer_menu_ibu_style_column.style.left = `${38 + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*10) + 4}px`
-    beer_menu_sqr_0305_column.style.left =  `${38 + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*10) + 4 + (prcnt_windowInnerWidth*10) - 5}px`
+    beer_menu_sqr_number_column.style.left = `${adapt_coeff}px`
+    beer_menu_sqr_name_column.style.left = `${38 + adapt_coeff}px`
+    beer_menu_sqr_brewery_column.style.left = `${38 + adapt_coeff + (prcnt_windowInnerWidth*20) + 4}px`
+    beer_menu_sqr_style_column.style.left = `${38 + adapt_coeff + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4}px`
+    beer_menu_abv_style_column.style.left = `${38 + adapt_coeff + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*18) + 4}px`
+    beer_menu_ibu_style_column.style.left = `${38 + adapt_coeff + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*10) + 4}px`
+    beer_menu_sqr_0305_column.style.left =  `${38 + adapt_coeff + (prcnt_windowInnerWidth*20) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*18) + 4 + (prcnt_windowInnerWidth*10) + 4 + (prcnt_windowInnerWidth*10) - 5}px`
     beer_menu_sqr_lines_vertical_2.style.left = `${26 + prcnt_windowInnerWidth*20}px`
     beer_menu_sqr_lines_vertical_3.style.left = `${26 + prcnt_windowInnerWidth*20 + (prcnt_windowInnerWidth*18)}px`
     beer_menu_sqr_lines_vertical_4.style.left =  `${26 + prcnt_windowInnerWidth*20 + (prcnt_windowInnerWidth*18) + (prcnt_windowInnerWidth*18) + 6}px`
