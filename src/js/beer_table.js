0907cd6446d04dc315f5ea5373ba5d98fcfd0c52
KosorukiyShiva <alcatelandry8@gmail.com>
2024-10-25 05:13:34 +0300

    poster map and business card are open, scripts are not written yet

--- src/js/beer_table.js
+++ src/js/beer_table.js
@@ -39,9 +39,9 @@ beer_menu_sqr_lines_horisontal_8.style.width = `${prcnt_windowInnerWidth * 88.3}
 
 beer_menu.style.width = `${prcnt_windowInnerWidth * 95.8}px`
 beer_menu_head.style.left = `${(prcnt_windowInnerWidth * 95.8 - 177) / 2}px`
-beer_menu_sqr.style.width = `${prcnt_windowInnerWidth * 95.8}px`
-beer_menu_sqr_button.style.left = `${(windowInnerWidth - 346)/2}px`
-if (windowInnerWidth >= 500) {
+beer_menu_sqr.style.width = `${prcnt_windowInnerWidth * 95.8-8}px`
+beer_menu_sqr_button.style.left = `${(prcnt_windowInnerWidth * 95.8 - 8 - 346)/2}px`
+if (windowInnerWidth >= 500) {Я
     beer_menu_abv_style_column.style.opacity = 1
     beer_menu_ibu_style_column.style.opacity = 1
 
@@ -86,7 +86,7 @@ if (windowInnerWidth < 500) {
     beer_menu_sqr_name_column.style.left = `${38}px`
     beer_menu_sqr_brewery_column.style.left = `${38 + (prcnt_windowInnerWidth * 24.2) + 8}px`
     beer_menu_sqr_style_column.style.left = `${38 + (prcnt_windowInnerWidth * 24.2) + 8 + (prcnt_windowInnerWidth*21.8) + 8}px`
-    beer_menu_sqr_0305_column.style.left =  `${38 + (prcnt_windowInnerWidth * 24.2) + 8 + (prcnt_windowInnerWidth*21.8) + 8 + (prcnt_windowInnerWidth*21.8) + 8}px`
+    beer_menu_sqr_0305_column.style.left =  `${38 + (prcnt_windowInnerWidth * 24.2) + 8 + (prcnt_windowInnerWidth*21.8) + 8 + (prcnt_windowInnerWidth*21.8) }px`
     beer_menu_sqr_lines_vertical_2.style.left = `${prcnt_windowInnerWidth * 29.6}px`
     beer_menu_sqr_lines_vertical_3.style.left = `${prcnt_windowInnerWidth * 53.4}px`
     beer_menu_sqr_lines_vertical_4.style.left =  `${prcnt_windowInnerWidth * 76.4}px`
