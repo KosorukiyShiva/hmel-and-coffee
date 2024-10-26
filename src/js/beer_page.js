70edd9db266bd18fc0014cfa05d0cae329cb2ea4
KosorukiyShiva <alcatelandry8@gmail.com>
2024-10-26 16:27:41 +0300

    inserted a beer menu layout, started writing a script for collapsing/expanding the snack table

---
+++ src/js/beer_page.js
@@ -0,0 +1,8 @@
+const coffe_menu_sqr_sirops_text = document.getElementById('coffe_menu_sqr_sirops_text')
+function beer_table(){
+    console.log('1234')
+    beer_menu.style.height = `${1005}px`
+    beer_menu_sqr.style.height = `${950}px`
+    beer_menu_sqr_button.style.top = `${870}px`
+    coffe_menu_sqr_sirops_text.textContent = '↑свернуть↑'
+}
