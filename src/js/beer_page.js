035a4face94957f2c56077910262a462258fe937
KosorukiyShiva <alcatelandry8@gmail.com>
2024-10-29 00:16:43 +0300

    I finally marked up the snack menu, there is a script for collapsing and expanding All that remains is to check the adaptation to the screen width

--- src/js/beer_page.js
+++ src/js/beer_page.js
@@ -2,14 +2,19 @@ const coffe_menu_sqr_sirops_text = document.getElementById('coffe_menu_sqr_sirop
 const beer_menu_sqr_snack = document.getElementById('beer_menu_sqr_snack')
 let open_state = false
 
+beer_menu.style.height = `${510}px`
+beer_menu_sqr.style.height = `${454}px`
+beer_menu_sqr_button.style.top = `${374}px`
+beer_menu_sqr_snack.style.height = `${0}px`
+
 function beer_table(){
     if(open_state === false){
         open_state = !open_state
         console.log(open_state)
-        beer_menu.style.height = `${1005}px`
-        beer_menu_sqr.style.height = `${950}px`
-        beer_menu_sqr_button.style.top = `${870}px`
-        beer_menu_sqr_snack.style.height = `${395}px`
+        beer_menu.style.height = `${925}px`
+        beer_menu_sqr.style.height = `${870}px`
+        beer_menu_sqr_button.style.top = `${790}px`
+        beer_menu_sqr_snack.style.height = `${420}px`
         coffe_menu_sqr_sirops_text.textContent = '↑свернуть↑'
         return
     }
@@ -24,17 +29,3 @@ function beer_table(){
         return
     }
 }
-
-
-
-
-// <ul>
-//                                     <li>
-                                        
-//                                     </li>
-//                                 </ul>
-//                                 ● Арахис................................100
-//                                 ● Арахис в оболочке..............150
-//                                 ● Гренки...……………...….…....150
-//                                 ● Вобла...………………...……....100
-//                                 ● Фисташки...……………...…....100
